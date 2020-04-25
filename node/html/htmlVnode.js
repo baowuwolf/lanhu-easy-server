87fb6f2b618373ab04da462b9d9c883ec1409896
zhukunpenglinyutong <270750933@qq.com>
2020-04-26 01:16:28 +0800

    第2次提交（01:16）| 上传第一版 功能版本代码

--- node/html/htmlVnode.js
+++ node/html/htmlVnode.js
@@ -14,16 +14,38 @@ function init(jsObject) {
 
 // 解析虚拟DOM（生成真实DOM）
 function parseVNode(vnode) {
+  return vnode.tag == 'img'
+    ? createImg(vnode)
+    : createDiv(vnode)
+}
+
+// 解析生成div
+function createDiv(vnode) {
   let _node = null
-  _node = document.createElement(vnode.tag || 'div')
+  _node = document.createElement('div')
   _node.setAttribute('class', vnode.className.split('.')[1])
-  
+  // 创建文字节点
+  if (vnode.innerText) {
+    let myText = document.createTextNode(vnode.innerText);
+    _node.appendChild(myText);
+  }
+
   // 子元素
   let children = vnode.children
   children.forEach(subvnode => {
     _node.appendChild(parseVNode(subvnode))
   })
 
+  return _node
+}
+
+// 解析生成img
+function createImg(vnode) {
+  // console.log('生成了 img', vnode)
+  let _node = null
+  _node = document.createElement('img')
+  _node.src = vnode.imgSrc;
+  _node.setAttribute('class', vnode.className.split('.')[1])
   return _node
 }
 
